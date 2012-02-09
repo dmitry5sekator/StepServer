72fd81bfe9a3171cd8fb6e97ff9a9e3c26c795a4
Ura <kyznetsovyurii@gmail.com>
2012-02-09 17:41:46 +0200

    2

---
+++ StepServer/Our/Response.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StepServer.our
+{
+    class Response
+    {
+        private string responseCode;
+        private string body;
+        string str = "HTTP/1.1 200 OK ";
+        public Response(string Code, String body)
+        {
+            setResponseCode(Code);
+            setBody(body);
+        }
+        public Response()
+        {
+
+        }
+        public string getResponseCode()
+        {
+            return responseCode;
+        }
+        public void setResponseCode(string responseCode)
+        {
+            this.responseCode = responseCode;
+        }
+        public string getBody()
+        {
+            return body;
+        }
+        public void setBody(String body)
+        {
+            this.body = body;
+        }
+
+        public String TOSTR()
+        {
+            return str + "code: " + responseCode.ToString() + " " + body;
+        }
+        public String toString()
+        {
+            //return str+ "Code: "+responseCode.toString()+"\tBody: "+body;
+            //Content-Type: text/xml
+            return str + "code: " + responseCode.ToString() + " " + body;
+        }
+        
+    }
+}
---
+++ StepServer/Our/UserRequest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace StepServer.our
+{
+    class UserRequest
+    {
+        private String body = "";
+        private String requestString = "";
+        private ArrayList param = new ArrayList();
+        public UserRequest()
+        {
+
+        }
+        public UserRequest(String requestString, String body, ArrayList param)
+        {
+            this.body = body;
+            this.requestString = requestString;
+            this.param = param;
+
+        }
+        public void setBody(String body)
+        {
+            this.body = body;
+
+        }
+        public void setRequestString(String requestString)
+        {
+            this.requestString = requestString;
+
+        }
+        public void setParam(ArrayList param)
+        {
+            this.param = param;
+
+        }
+        public String getBody()
+        {
+            return body;
+        }
+        public String getRequestString()
+        {
+            return requestString;
+        }
+
+        public String getParam()
+        {
+            return requestString;
+        }
+     //   public String AuthLogin()
+     //{
+     //    String temp = "";
+     //    for(int i = 0;i < param.size();i++)
+     //    {
+     //        temp = param.get(i).substring(0, 7);
+     //        if(temp.equalsIgnoreCase("Login: "))
+     //        {
+     //            System.out.println("check");
+     //            return param.get(i).substring(param.get(i).indexOf("Login: ")+7,param.get(i).length());
+     //        }
+     //    }
+     //   return "";
+     //}
+     //   public String AuthPassword()
+     //{
+     //    String temp = "";
+     //    for(int i = 0;i < param.size();i++)
+     //    {
+     //        temp = param.get(i).substring(0, 10);
+     //        if(temp.equalsIgnoreCase("Password: "))
+     //        {
+     //            System.out.println("check");
+     //            return param.get(i).substring(param.get(i).indexOf("Login: ")+11,param.get(i).length());
+     //        }
+     //    }
+     //   return "";
+     //}
+    //    public String AuthMD5()
+    // {
+    //     String temp = "";
+    //     for(int i = 0;i < param.size();i++)
+    //     {
+    //         temp = param.get(i).substring(0, 5);
+    //         if(temp.equalsIgnoreCase("MD5: "))
+    //         {
+    //             System.out.println("check");
+    //             return param.get(i).substring(param.get(i).indexOf("Login: ")+6,param.get(i).length());
+    //         }
+    //     }
+    //    return "";
+    // }
+   }
+}
